598fa6edd0824550d46cd6845f44ac05a6befd2f
int2itive <70378581+int2itive@users.noreply.github.com>
2024-07-31 15:02:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const headerH4 = document.querySelector('.creatives .content p');
+
+const text = new SplitType(headerH4, { types: 'words, chars' })
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuSlider = document.querySelector('.mbl--list-items');
+
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  // alert("Hwllo!");
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuSlider.classList.add('showing');
+    gsap.from('.mbl--list-items .item a', { duration: 1, opacity: 0, delay: .75, stagger: 0.3 });
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuSlider.classList.remove('showing');
+    menuOpen = false;
+  }
+});
+
+gsap.from('.item a', { duration: 1, opacity: 0, delay: 1, stagger: 0.4 });
+
+gsap.fromTo(text.chars, {
+  scaleY: 0.1,
+  scaleX: 1.8,
+  filter: 'blur(10px) brightness(50%)',
+  willChange: 'filter, transform'
+}, {
+    ease: 'none', 
+    scaleY: 1,
+    scaleX: 1,
+    filter: 'blur(0px) brightness(100%)',
+    stagger: 0.03, 
+    scrollTrigger: {
+      trigger: text,
+      start: 'top bottom-=15%', 
+      end: 'bottom center+=15%',
+      scrub: true, 
+    },
+});
